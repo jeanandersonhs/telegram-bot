cccf096e1286e8fe997717ba7ec22043b9e447d7
jeanandersonhs <jeanhugo2016@gmail.com>
2025-05-09 11:42:53 -0300

    inicial commit

---
+++ bot.py
@@ -0,0 +1,41 @@
+import logging
+from telegram import Update
+
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+import os 
+from dotenv import load_dotenv 
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN-API-TELEGRAM")
+
+# funcao processa a atualizacao
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="bem vindoooo ao bot"
+    )
+
+
+def main ():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(start_handler)
+
+
+    app.run_polling()
+    print()
+
+
+
+
+
+if __name__ == "__main__": 
+    main()
